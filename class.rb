7c8fcee0fddc55dd71250366eb8a5b7777fd1adf
VitD <gektop555@mail.ru>
2013-11-13 20:06:13 +0700

    3

--- class.rb
+++ class.rb
@@ -1,5 +1,6 @@
 class Car
-attr_reader :options
+attr_reader :model, :make, :engine_volume, :year_of_release, :transmission, :fuel,
+:steering_wheel
 
 	  def initialize (opts={})
 	@model = opts [:model]
@@ -22,15 +23,37 @@ attr_reader :options
 	   puts "fuel: #{fuel}"
 	   puts "steering_wheel: #{steering_wheel}"
 	  end
+	  
+	  def gps
+		
 end
 c1 = Car.new(
 	:model => "GAZ 2705 264",
 	:make => "Russia",
-	:engine_volume => 2800,
+	:engine_volume => 280,
 	:year_of_release => 2013,
 	:transmission => "Mechanical",
 	:fuel => "Gasoline",
 	:steering_wheel => "left"
 	)
-
+c2 = Car.new(
+	:model => "Jeep Grand Cherokee",
+	:make => "USA",
+	:engine_volume => 234,
+	:year_of_release => 2004,
+	:transmission => "Automat",
+	:fuel => "Gasoline",
+	:steering_wheel => "left"
+	)
+c3 = Car.new(
+	:model => "MAN LION'S CITY (A78)",
+	:make => "Russia",
+	:engine_volume => 280,
+	:year_of_release => 2010,
+	:transmission => "Mechanical",
+	:fuel => "Gasoline",
+	:steering_wheel => "left"
+	)
 c1.show
+c2.show
+c3.show
